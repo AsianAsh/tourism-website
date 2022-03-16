f7024ca309130282e60798aef4be54d696c265a8
aimaniskandar <aimaniskandar5858@gmail.com>
2022-03-17 01:47:54 +0800

    v1.2 admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -8,76 +8,101 @@ if (!isset($_SESSION["admin"]["adminID"])) {
     exit();
 }
 ?>
+<<!DOCTYPE html>
 
-<!-- Page Header-->
-<header class="section page-header">
-    
-    <!-- RD Navbar-->
-    <div class="rd-navbar-wrap">
-        <nav class="rd-navbar rd-navbar-corporate" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="106px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
-        <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
-        <div class="rd-navbar-aside-outer">
-            <div class="rd-navbar-aside">
-            <!-- RD Navbar Panel-->
-            <div class="rd-navbar-panel">
-                <!-- RD Navbar Toggle-->
-                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
-                <!-- RD Navbar Brand-->
-                <div class="rd-navbar-brand">
-                <!--Brand--><a class="brand" href="index.html"><img src="images/logo-default-450x37.png" alt="" width="225" height="18"/></a>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="dashboard.css">
+        <!-- Boxicons CDN Link -->
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    <div class="sidebar">
+        <div class="logo-details">
+        <!-- image -->
+        <i class='bx bxl-c-plus-plus'></i>
+        <span class="logo_name">Travel Time</span>
+        </div>
+        <ul class="nav-links">
+            <li>
+            <a href="#" class="active">
+                <i class='bx bx-grid-alt' ></i>
+                <span class="links_name">Dashboard</span>
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                <i class='bx bx-user' ></i>
+                <span class="links_name">Create Staff Account</span>
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                <i class='bx bx-user' ></i>
+                <span class="links_name">Create Agent Account </span>
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                <i class='bx bx-coin-stack' ></i>
+                <span class="links_name">Sales Report</span>
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                <i class='bx bx-log-out' ></i>
+                <span class="links_name">Log Out</span>
+            </a>
+        </ul>
+    </div>
+    <section class="home-section">
+        <nav>
+        <div class="sidebar-button">
+            <i class='bx bx-menu sidebarBtn'></i>
+            <span class="dashboard">Home</span>
+        </div>
+        
+        </nav>
+
+        <div class="home-content">
+        <div class="overview-boxes">
+            <div class="box">
+            <div class="right-side">
+                <div class="box-topic">Latest</div>
+                <div class="number"></div>
+                <div class="indicator">
+            
+                <span class="text">News In malaysia</span>
                 </div>
             </div>
             </div>
-
+    
         </div>
-        <div class="rd-navbar-main-outer">
-            <div class="rd-navbar-main">
-            <div class="rd-navbar-nav-wrap">
-                <ul class="list-inline list-inline-md rd-navbar-corporate-list-social">
-                <li><a class="icon fa fa-facebook" href="#"></a></li>
-                <li><a class="icon fa fa-twitter" href="#"></a></li>
-                <li><a class="icon fa fa-google-plus" href="#"></a></li>
-                <li><a class="icon fa fa-instagram" href="#"></a></li>
-                </ul>
 
-                <!-- Account Button -->
-                <ul>
-                <li class="rd-nav-item">
-                    <a class="rd-nav-link" data-bs-toggle="offcanvas" href="#accountCanvas" role="button" aria-controls="accountCanvas">Account</a>
-                </li>
-                </ul>
-
-                <!-- RD Navbar Nav-->
-                <ul class="rd-navbar-nav">
-
-                <!-- Find a way to have the tab of the current page highlighted (rd-nav-item active) 
-                without having to cut & paste this entire <ul> code in each php file-->
-                <!-- <li class="rd-nav-item active"><a class="rd-nav-link" href="index.php">Home</a> 
-                </li> -->
-
-                <li class="rd-nav-item"><a class="rd-nav-link" href="admin_dashboard.php">Welcome Admin</a>
-                </li>
-                <li class="rd-nav-item"><a class="rd-nav-link" href="about.php"></a>
-                </li>
-                <li class="rd-nav-item"><a class="rd-nav-link" href="tours.php"></a>
-                </li>
-                <li class="rd-nav-item"><a class="rd-nav-link" href="typography.php"></a>
-                </ul>
-
+        <div class="sales-boxes">
+            <div class="recent-sales box">
+            <!-- <div class="title">Whats Hot?</div> -->
+            
             </div>
             </div>
-        </div>
-        </nav>
-    </div>
-</header>
+            
+    <script>
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".sidebarBtn");
+    sidebarBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+    if(sidebar.classList.contains("active")){
+    sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+    }else
+    sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+    </script>
 
-<div class="d-flex justify-content-center" style="padding:50px 0px 50px 0px ">
-    <a type="button" class="btn-lg btn-primary mx-3" href="register_staff.php">Create Office Staff Account</a>
-    <a type="button" class="btn-lg btn-primary mx-3" href="register_agent.php">Create Travel Agent Account</a>
-    <a type="button" class="btn-lg btn-primary mx-3" href="sales.php">View Sales Report</a>
-</div>
-
-
+</body>
+</html>
 <?php require_once "./components/scripts.php"; ?>
 
 
