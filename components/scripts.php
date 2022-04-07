b32702e74da9cd22192bdc06a80bf53054f98c28
Char Lap Chun <91192122+AsianAsh@users.noreply.github.com>
2022-04-08 04:20:10 +0800

    Final Commit

--- components/scripts.php
+++ components/scripts.php
@@ -6,7 +6,6 @@
     <script src="js/login_show_password.js"></script>
     <script src="js/current_date.js"></script>
     <script src="js/checkin_date.js"></script>
-    <script src="js/profile.js"></script>
     <!-- Bootstrap -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
